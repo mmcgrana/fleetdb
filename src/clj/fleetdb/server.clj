cd9f726087a5b495182d48c3f0a53c43182ade78
Mark McGranaghan <mmcgrana@gmail.com>
2009-11-23 23:11:09 -0500

    Threaded and more robust text protocol server.

--- src/clj/fleetdb/server.clj
+++ src/clj/fleetdb/server.clj
@@ -1,5 +1,5 @@
 (ns fleetdb.server
-  (:require (fleetdb [embedded :as embedded] [io :as io])
+  (:require (fleetdb [embedded :as embedded] [io :as io] [exec :as exec])
             (clj-stacktrace [repl :as stacktrace]))
   (:import  (java.net ServerSocket Socket)
             (java.io PrintWriter BufferedReader InputStreamReader PushbackReader)))
@@ -11,6 +11,7 @@
 
 (defn- write-exception [#^PrintWriter out e]
   (stacktrace/pst-on out false e)
+  (.println out)
   (.flush out))
 
 (defn- write-result [#^PrintWriter out result]
@@ -19,30 +20,36 @@
 
 (defn- safe-query [dba query]
   (try
-    (embedded/query dba query)
+    (let [result (embedded/query dba query)]
+      (if (coll? result) (doall result) result))
     (catch Exception e e)))
 
-(defn- handle-text [dba #^Socket socket]
-  (with-open [socket socket
-              out    (PrintWriter. (.getOutputStream socket))
-              in     (PushbackReader. (BufferedReader. (InputStreamReader. (.getInputStream socket))))]
-    (loop []
-      (let [read-result (read-query in io/eof)]
-        (when-not (identical? io/eof read-result)
-          (if (instance? Exception read-result)
-            (write-exception out read-result)
-            (let [query-result (safe-query dba read-result)]
-              (if (instance? Exception query-result)
-                (write-exception out query-result)
-                (write-result out query-result))))
-          (recur))))))
+(defn- text-handler [dba #^Socket socket]
+  (try
+    (with-open [socket socket
+                out    (PrintWriter. (.getOutputStream socket))
+                in     (PushbackReader. (BufferedReader. (InputStreamReader. (.getInputStream socket))))]
+      (loop []
+        (let [read-result (read-query in io/eof)]
+          (when-not (identical? io/eof read-result)
+            (if (instance? Exception read-result)
+              (write-exception out read-result)
+              (let [query-result (safe-query dba read-result)]
+                (if (instance? Exception query-result)
+                  (write-exception out query-result)
+                  (write-result out query-result))))
+            (recur)))))
+    (catch Exception e
+      (stacktrace/pst-on System/err true e))))
 
-(defn run [read-path write-path port]
-  (let [server-socket (ServerSocket. port)
-        dba           (embedded/init read-path write-path)]
-    (println "FleetDB serving port" port)
+(defn run [read-path write-path text-port]
+  (let [text-ss (ServerSocket. text-port)
+        dba     (embedded/init read-path write-path)
+        pool    (exec/init-pool 100)]
+    (println "FleetDB serving text protocol from port" text-port)
     (loop []
-      (handle-text dba (.accept server-socket))
+      (let [socket (.accept text-ss)]
+        (exec/submit pool #(text-handler dba socket)))
       (recur))))
 
 (use 'clojure.contrib.shell-out)
