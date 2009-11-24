8f75576f03ee3ddd22340120eac744ba7b334fd0
Mark McGranaghan <mmcgrana@gmail.com>
2009-11-23 23:51:48 -0500

    Implement binary protocol server, tighten error handling.

---
+++ scratch/server_test.clj
@@ -0,0 +1,15 @@
+(set! *warn-on-reflection* true)
+
+(require '(fleetdb [io :as io]))
+(import '(java.net Socket)
+        '(java.io DataInputStream BufferedInputStream DataOutputStream BufferedOutputStream))
+
+(let [socket (Socket. "localhost" 4444)
+      dis    (DataInputStream.  (BufferedInputStream.  (.getInputStream  socket)))
+      dos    (DataOutputStream. (BufferedOutputStream. (.getOutputStream socket)))]
+  (io/dos-write dos [:select])
+  (prn (io/dis-read dis io/eof))
+  (io/dos-write dos [:insert {:records [{:id 1 :name "mark"} {:id 2 :name "laura"}]}])
+  (prn (io/dis-read dis io/eof))
+  (io/dos-write dos [:select])
+  (prn (io/dis-read dis io/eof)))
--- src/clj/fleetdb/server.clj
+++ src/clj/fleetdb/server.clj
@@ -2,56 +2,91 @@
   (:require (fleetdb [embedded :as embedded] [io :as io] [exec :as exec])
             (clj-stacktrace [repl :as stacktrace]))
   (:import  (java.net ServerSocket Socket)
-            (java.io PrintWriter BufferedReader InputStreamReader PushbackReader)))
-
-(defn- read-query [#^PushbackReader in eof-val]
-  (try
-    (read in false eof-val)
-    (catch Exception e e)))
-
-(defn- write-exception [#^PrintWriter out e]
-  (stacktrace/pst-on out false e)
-  (.println out)
-  (.flush out))
-
-(defn- write-result [#^PrintWriter out result]
-  (.println out (prn-str result))
-  (.flush out))
+            (java.io PushbackReader BufferedReader InputStreamReader
+                     PrintWriter    BufferedWriter OutputStreamWriter
+                     DataInputStream  BufferedInputStream
+                     DataOutputStream BufferedOutputStream )))
 
 (defn- safe-query [dba query]
   (try
     (let [result (embedded/query dba query)]
-      (if (coll? result) (doall result) result))
+      (if (coll? result) (vec result) result))
     (catch Exception e e)))
+
+(defn- text-read-query [#^PushbackReader in eof-val]
+  (try
+    (read in false eof-val)
+    (catch Exception e e)))
+
+(defn- text-write-exception [#^PrintWriter out e]
+  (stacktrace/pst-on out false e)
+  (.println out)
+  (.flush out))
+
+(defn- text-write-result [#^PrintWriter out result]
+  (.println out (prn-str result))
+  (.flush out))
 
 (defn- text-handler [dba #^Socket socket]
   (try
     (with-open [socket socket
-                out    (PrintWriter. (.getOutputStream socket))
-                in     (PushbackReader. (BufferedReader. (InputStreamReader. (.getInputStream socket))))]
+                out    (PrintWriter.    (BufferedWriter. (OutputStreamWriter. (.getOutputStream socket))))
+                in     (PushbackReader. (BufferedReader. (InputStreamReader.  (.getInputStream  socket))))]
       (loop []
-        (let [read-result (read-query in io/eof)]
+        (let [read-result (text-read-query in io/eof)]
           (when-not (identical? io/eof read-result)
             (if (instance? Exception read-result)
-              (write-exception out read-result)
+              (text-write-exception out read-result)
               (let [query-result (safe-query dba read-result)]
                 (if (instance? Exception query-result)
-                  (write-exception out query-result)
-                  (write-result out query-result))))
+                  (text-write-exception out query-result)
+                  (text-write-result out query-result))))
             (recur)))))
     (catch Exception e
-      (stacktrace/pst-on System/err true e))))
+      (stacktrace/pst-on System/err false e)
+      (.println System/err))))
 
-(defn run [read-path write-path text-port]
-  (let [text-ss (ServerSocket. text-port)
+(defn- binary-read-query [#^DataInputStream in eof-val]
+  (try
+    (io/dis-read in eof-val)
+    (catch Exception e e)))
+
+(defn- binary-write-exception [#^DataOutputStream out e]
+  (io/dos-write [1 (stacktrace/pst-str e)]))
+
+(defn- binary-write-result [#^DataOutputStream out result]
+  (io/dos-write out [0 result]))
+
+(defn- binary-handler [dba #^Socket socket]
+  (try
+    (with-open [socket socket
+                out    (DataOutputStream. (BufferedOutputStream. (.getOutputStream socket)))
+                in     (DataInputStream.  (BufferedInputStream.  (.getInputStream  socket)))]
+      (loop []
+        (let [read-result (binary-read-query in io/eof)]
+          (when-not (identical? io/eof read-result)
+            (if (instance? Exception read-result)
+              (binary-write-exception out read-result)
+              (let [query-result (safe-query dba read-result)]
+                (if (instance? Exception query-result)
+                  (binary-write-exception out query-result)
+                  (binary-write-result out query-result))))
+            (recur)))))
+    (catch Exception e
+      (stacktrace/pst-on System/err false e)
+      (.println System/err))))
+
+(defn run [read-path write-path port binary]
+  (let [text-ss (ServerSocket. port)
         dba     (embedded/init read-path write-path)
-        pool    (exec/init-pool 100)]
-    (println "FleetDB serving text protocol from port" text-port)
+        pool    (exec/init-pool 100)
+        handler (if binary binary-handler text-handler)]
+    (println "FleetDB: serving port" port)
     (loop []
       (let [socket (.accept text-ss)]
-        (exec/submit pool #(text-handler dba socket)))
+        (exec/submit pool #(handler dba socket)))
       (recur))))
 
 (use 'clojure.contrib.shell-out)
 (sh "rm" "-f" "/Users/mmcgrana/Desktop/log")
-(run nil "/Users/mmcgrana/Desktop/log" 4444)
+(run nil "/Users/mmcgrana/Desktop/log" 4444 true)
