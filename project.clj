e52123993177974333a90fda44f8b68583b49384
Mark McGranaghan <mmcgrana@gmail.com>
2010-01-13 09:29:05 -0800

    Updated Clojure dependencies.

--- project.clj
+++ project.clj
@@ -1,15 +1,15 @@
-(defproject fleetdb "0.1.0-SNAPSHOT"
-  :description "A document-oriented database optimized for agile development."
+(defproject fleetdb "0.1.0"
+  :description "A schema-free database optimized for agile development."
   :url "http://github.com/mmcgrana/fleetdb"
   :source-path "src/clj"
   :java-source-path "src/jvm/"
   :javac-fork "true"
-  :dependencies [[org.clojure/clojure "1.1.0-master-SNAPSHOT"]
-                 [org.clojure/clojure-contrib "1.0-SNAPSHOT"]
+  :dependencies [[org.clojure/clojure "1.1.0"]
+                 [org.clojure/clojure-contrib "1.1.0-master-SNAPSHOT"]
                  [clj-stacktrace "0.1.0-SNAPSHOT"]
                  [net.sf.jopt-simple/jopt-simple "3.2"]
                  [clj-json "0.1.0-SNAPSHOT"]]
   :dev-dependencies [[lein-clojars "0.5.0-SNAPSHOT"]
                      [clj-unit "0.1.0-SNAPSHOT"]
                      [lein-javac "0.0.2-SNAPSHOT"]
-                     [fleetdb-client "0.1.0-SNAPSHOT"]])
+                     [fleetdb-client "0.1.0"]])
