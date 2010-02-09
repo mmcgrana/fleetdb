1cd9a3b05dacabbc1c2de2730b01f71518a288f6
Mark McGranaghan <mmcgrana@gmail.com>
2010-02-09 10:55:11 -0500

    Declare that fleetdb.server should be compiled, for newer versions of lein.

--- project.clj
+++ project.clj
@@ -11,4 +11,5 @@
                  [clj-json "0.1.0-SNAPSHOT"]]
   :dev-dependencies [[lein-clojars "0.5.0-SNAPSHOT"]
                      [clj-unit "0.1.0-SNAPSHOT"]
-                     [lein-javac "0.0.2-SNAPSHOT"]])
+                     [lein-javac "0.0.2-SNAPSHOT"]]
+  :namespaces [fleetdb.server])
