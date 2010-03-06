5881ba4d7351a793d8e1c7042f9b0640efc544eb
Mark McGranaghan <mmcgrana@gmail.com>
2010-03-06 11:25:57 -0500

    Bump version to 0.2.0-RC1.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject fleetdb "0.1.1-SNAPSHOT"
+(defproject fleetdb "0.2.0-RC1"
   :description "A schema-free database optimized for agile development."
   :url "http://github.com/mmcgrana/fleetdb"
   :source-path "src/clj"
@@ -8,8 +8,8 @@
                  [org.clojure/clojure-contrib "1.1.0"]
                  [clj-stacktrace "0.1.0"]
                  [net.sf.jopt-simple/jopt-simple "3.2"]
-                 [clj-json "0.2.0-SNAPSHOT"]]
-  :dev-dependencies [[lein-clojars "0.5.0-SNAPSHOT"]
-                     [clj-unit "0.1.0-SNAPSHOT"]
-                     [lein-javac "0.0.2-SNAPSHOT"]]
+                 [clj-json "0.2.0"]]
+  :dev-dependencies [[org.clojars.mmcgrana/lein-clojars "0.5.0"]
+                     [org.clojars.mmcgrana/lein-javac "0.1.0"]
+                     [clj-unit "0.1.0"]]
   :namespaces [fleetdb.server])
