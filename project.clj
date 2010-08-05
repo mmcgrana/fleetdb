3d9010fe2f0753a70f099edb3aff8abb3c269a32
Mark McGranaghan <mmcgrana@gmail.com>
2010-08-05 10:43:55 -0700

    Bump dependency versions.

--- project.clj
+++ project.clj
@@ -4,12 +4,11 @@
   :source-path "src/clj"
   :java-source-path "src/jvm/"
   :javac-fork "true"
-  :dependencies [[org.clojure/clojure "1.1.0"]
-                 [org.clojure/clojure-contrib "1.1.0"]
+  :dependencies [[org.clojure/clojure "1.2.0-RC1"]
+                 [org.clojure/clojure-contrib "1.2.0-RC1"]
                  [clj-stacktrace "0.1.2"]
                  [net.sf.jopt-simple/jopt-simple "3.2"]
                  [clj-json "0.2.0"]]
-  :dev-dependencies [[org.clojars.mmcgrana/lein-clojars "0.5.0"]
-                     [org.clojars.mmcgrana/lein-javac "0.1.0"]
+  :dev-dependencies [[lein-javac "1.2.1-SNAPSHOT"]
                      [clj-unit "0.1.0"]]
   :namespaces [fleetdb.server])
